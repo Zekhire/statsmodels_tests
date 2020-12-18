b438e209201dd6fbf501360365eebcce009cf913
Zekhire <krzysztofprzem@gmail.com>
2020-12-18 13:21:50 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -8,17 +8,18 @@ import numpy as np
 
 
 dt = 0.01
-data = [x + np.random.normal(1, 1) for x in range(50)]
-t = [x*dt for x in range(len(data))]
+data = [x + np.random.normal(1, 1) for x in range(50)]          # <- stworz wektor probek procesu
+t = [x*dt for x in range(len(data))]                            # <- stworz wektor czasu
 
+# w kazdej iteracji doloz kolejna probke procesu, zeby wyznaczyc model
 for i in range(8, len(data)):
     # model = AutoReg(data[:i], lags=3, old_names=False)
     # model = ARIMA(data[:i], order=(2, 0, 1))
-    model = ARIMA(data[:i], order=(2, 1, 1))
+    model = ARIMA(data[:i], order=(2, 1, 1))                    # <- wyznacz model bazujac na i pierwszych probkach procesu
     model_fit = model.fit()
     yhat = model_fit.predict(1, len(data))
     shift = 1
-    ty = [j*dt for j in range(shift, len(yhat)+shift)]
+    ty = [j*dt for j in range(shift, len(yhat)+shift)]          # <- wyznacz wektor czasu dla przewidywanych probek
     print(yhat)
     
     plt.plot(t, data)
